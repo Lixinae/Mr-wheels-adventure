880cbdbec7e1f240b15fb236499c8b3a05401e21
Vahen <naeos@laposte.net>
2017-08-21 17:30:37 +0200

    Ajout d'un cube

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,16 +16,22 @@ public class PlayerController : MonoBehaviour {
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
+		// Limite de vitesse sur l'axe X
         if (rb.velocity.x > 10 || rb.velocity.x < -10)
         {
             moveHorizontal = 0;
         }
+
+		// Limite de vitesse sur l'axe Z
         if(rb.velocity.z > 10 || rb.velocity.z < -10)
         {
             moveVertical = 0;
         }
-            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-        rb.AddForce(movement * speed);
-        print("velocity :"+rb.velocity.x+" "+ rb.velocity.y+" "+ rb.velocity.z);
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rb.AddForce(movement * speed);
+		print("velocity :"+rb.velocity.x+" "+ rb.velocity.y+" "+ rb.velocity.z);  
+
     }
 }
