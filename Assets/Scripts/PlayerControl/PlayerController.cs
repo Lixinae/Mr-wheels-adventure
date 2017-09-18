69253909f9438d54a7481d83fb0e970d75900f16
kevblond <kevinblondel@msn.com>
2017-09-18 17:40:03 +0200

    essaie jump problème multi saut

--- Assets/Scripts/PlayerControl/PlayerController.cs
+++ Assets/Scripts/PlayerControl/PlayerController.cs
@@ -4,34 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public Rigidbody rb;
+
+    //saut
+    [SerializeField]
+    private Transform _transform;
+
+    [SerializeField]
+    private float _jumpSpeed;
+
+    [SerializeField]
+    private Rigidbody _rigidbody;
+
+    private float _feetRadius;
+
+    private RaycastHit _hit;
+
+    private bool _canJump = true;
+
+    //mouvement
     public float speed;
+
+
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _feetRadius = GetComponent<SphereCollider>().radius;
     }
 
     void FixedUpdate()
     {
+        //saut
+        //print("test "+_canJump +" " + Input.GetAxis("Jump") + " " + _transform.position +" \n");
+        print(Physics.SphereCast(_transform.position + Vector3.up * _feetRadius * 1.1f, _feetRadius, Vector3.down, out _hit, 0.1f));
+        //print(_transform.position + Vector3.up * _feetRadius * 1.1f +" "+ _transform.position);
+        if (!_canJump && Input.GetAxis("Jump") == 0)
+        {
+            _canJump = true;
+        }
+        if (_canJump && Input.GetAxis("Jump") > 0 && Physics.SphereCast(_transform.position + Vector3.up * _feetRadius * 1.1f, _feetRadius, Vector3.down, out _hit, 0.1f))
+        {
+            print("test2\n");
+            _rigidbody.AddForce(Vector3.up * _jumpSpeed, ForceMode.VelocityChange);
+            _canJump = false;
+        }
+
+
+        //mouvement horizontal
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
 		// Limite de vitesse sur l'axe X
-        if (rb.velocity.x > 10 || rb.velocity.x < -10)
+        if (_rigidbody.velocity.x > 10 || _rigidbody.velocity.x < -10)
         {
             moveHorizontal = 0;
         }
 
 		// Limite de vitesse sur l'axe Z
-        if(rb.velocity.z > 10 || rb.velocity.z < -10)
+        if(_rigidbody.velocity.z > 10 || _rigidbody.velocity.z < -10)
         {
             moveVertical = 0;
         }
 
 		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-		rb.AddForce(movement * speed);
-		print("velocity :"+rb.velocity.x+" "+ rb.velocity.y+" "+ rb.velocity.z);  
+        _rigidbody.AddForce(movement * speed);
+		//print("velocity :"+rb.velocity.x+" "+ rb.velocity.y+" "+ rb.velocity.z);  
 
     }
 }
